eae3a879169f20743938e647c7e5a1d01de9a295
QuangCao-Art <98384247+QuangCao-Art@users.noreply.github.com>
2026-04-20 11:23:25 +0700

    udpate before refactor wall and floor collistion

--- src/engine/overworld.js
+++ src/engine/overworld.js
@@ -668,7 +668,7 @@ export const Overworld = {
 
                 const isClosedDoor = [20, 22, 24, 25, 28, 29, 30, 31, 39, 40, 41, 42].includes(tileID);
                 const isOpenDoor = [21, 23, 26, 27, 34, 35, 36, 37].includes(tileID);
-                const isGenericWall = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 32, 33, 43, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63].includes(tileID);
+                const isGenericWall = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 32, 33, 43, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63].includes(tileID);
 
                 if (isGenericWall || isClosedDoor) tile.classList.add('wall');
                 else tile.classList.add('floor');
